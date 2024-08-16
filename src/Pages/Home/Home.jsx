38baee184f60227322bb595825fb9fea6d53557b
SanimSRk <hasansanim562@gmail.com>
2024-08-16 07:30:34 +0600

    search product name is done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-import { RiArrowDropDownLine } from 'react-icons/ri';
 import Banner from '../Banner/Banner';
 import useAxiosPublice from '../../Hooks/useAxiosPublice';
 import { useState } from 'react';
@@ -27,36 +26,51 @@ const Home = () => {
   const [selectedOption1, setSelectedOption1] = useState(null);
   const [selectedOption2, setSelectedOption2] = useState(null);
   const [selectedOption3, setSelectedOption3] = useState(null);
-  const { data: productData } = useQuery({
+
+  const { data } = useQuery({
     queryKey: ['products'],
     queryFn: async () => {
       const { data } = await axiosPublice.get('/product-data');
+      setShowsProduct(data);
       return data;
     },
   });
-  console.log(productData);
+
+  const handileClickSearch = e => {
+    e.preventDefault();
+    console.log(e.target.search.value);
+    const search = e.target.search.value;
+    axiosPublice.get(`/search-products?search=${search}`).then(res => {
+      console.log(res.data);
+      setShowsProduct(res.data);
+    });
+  };
+
+  console.log(showsProduct);
   return (
     <div className="mt-1 mb-[100px]">
       <div>
         <Banner></Banner>
       </div>
-      <div className="navbar  bg-[#D35400] lg:w-2/3 justify-center  mx-auto mt-2">
+      <div className="navbar  bg-[#D35400]  justify-center  mx-auto mt-2">
         <div className="">
-          {/* <div className=" flex items-center">
-            <input
-              className="shadow appearance-none  border-2 rounded-lg w-full  md:w-[300px] lg:w-[380px]  input input-bordered text-gray-700 leading-tight focus:outline-none rounded-r-none"
-              id="search"
-              type="search"
-              name="search"
-              placeholder="Search your products......"
-            />
-            <button
-              type="submit"
-              className="btn  bg-[#2C3E50] text-whitess text-white font-semibold rounded-l-none"
-            >
-              Search
-            </button>
-          </div> */}
+          <form onSubmit={handileClickSearch}>
+            <div className=" flex items-center">
+              <input
+                className="shadow appearance-none  border-2 rounded-lg w-full  md:w-[300px] lg:w-[380px]  input input-bordered text-gray-700 leading-tight focus:outline-none rounded-r-none"
+                id="search"
+                type="search"
+                name="search"
+                placeholder="Search your products......"
+              />
+              <button
+                type="submit"
+                className="btn  bg-[#2C3E50] text-whitess text-white font-semibold rounded-l-none"
+              >
+                Search
+              </button>
+            </div>
+          </form>
         </div>
         <div className="gap-3 lg:gap-4">
           <Select
@@ -83,7 +97,7 @@ const Home = () => {
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 mt-6 lg:grid-cols-3 gap-6">
-        {productData.map(product => (
+        {showsProduct?.map(product => (
           <ProductCard key={product._id} product={product}></ProductCard>
         ))}
       </div>
